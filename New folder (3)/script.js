a9bd19db1707ef31b8e45d6ac7fdb5aafa759a18
Rajath2005 <168326104+Rajath2005@users.noreply.github.com>
2024-08-26 21:08:24 +0530

    Add files via upload

---
+++ New folder (3)/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Set the date we're counting down to (tomorrow)
+    var countDownDate = new Date("Aug 27, 2024 9:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
